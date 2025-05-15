998c87f7249be92f735dcb70fd2fcbd02d09848c
Ana Vargas <anivargas1992@gmail.com>
2025-05-15 21:11:17 +0200

    authentication with pinia ok

--- src/views/authetication.vue
+++ src/views/authetication.vue
@@ -1,71 +1,35 @@
 <script setup>
-import {ref} from 'vue';
-import {supabase} from '@/api/supabase/index.js'
+import {useUserStore} from '@/stores/user'
+import {ref} from 'vue' 
+import {storeToRefs} from 'pinia'
+
+const userStore = useUserStore()
+const {user} = storeToRefs(userStore)
 
 const email = ref("")
 const name = ref("")
 const password = ref("")
 
-async function signIn() {  
-    const { data, error } = await supabase.auth.signUp({
-        email: email.value,
-        password: password.value,
-        options: {
-            data: {name: name.value}
-        }
-    }) 
-    if (error){
-        console.error (error);
-    }
-    else{
-        console.log (data); 
-    }
-}
-async function logIn() { const { data, error } = await supabase.auth.signInWithPassword({
-        email: email.value,
-        password: password.value,
-    }) 
-    if (error){
-        console.error (error);
-    }
-    else{
-        console.log (data); 
-    }    
-}
-async function getUser() {
-    const localUser= await supabase.auth.getSession();
-    console.log (localUser);
-    
-}
-async function logout() {    
-    const { error } = await supabase.auth.signOut(); 
-    if (error) {
-        console.log (error)
-    }  
-    else {
-        console.log ("you have log out")
-    }
-}
-
 
 </script>
 <template>
     <div>
-        <form action="" method="post">
-            <label for="Email">Email:</label>
-            <input type="text" id="Email" name="Email" v-model="email" required><br><br>
-
-            <label for="name">Nombre:</label>
-            <input type="text" id="name" name="name" v-model="name" required><br><br>
-
-
-            <label for="password">Contraseña:</label>
-            <input type="password" id="password" name="password" v-model="password" required><br><br>
-
-            <button @click="signIn" type="button">Registrate</button>
-            <button @click="logIn"  type="button">Iniciar Sesión</button>
-            <button @click="logout"  type="button">Cerrar Sesión</button>
-            <button @click="getUser"  type="button">ver sesion</button>
-            </form>
+      <form @submit.prevent>
+        <label for="email">Email:</label>
+        <input type="text" id="email" v-model="email" required><br><br>
+       
+        <label for="password">Contraseña:</label>
+        <input type="password" id="password" v-model="password" required><br><br>
+  
+        <button @click="userStore.signUp(email, password)" type="button">Registrarse</button>
+        <button @click="userStore.logIn(email, password)" type="button">Iniciar Sesión</button>
+      </form>
+  
+      <div v-if="user">
+        <h3>Estas conectado como:</h3>
+        <p>{{ user.email }}</p>
+        <button @click="userStore.logout" type="button">Cerrar Sesión</button>
+      </div>
     </div>
-</template>
+  </template>
+  
